ec5e84bed4a3c6bf52e37578bba8344404de19b8
Crowdbotics <team@crowdbotics.com>
2023-10-06 14:52:53 +0000

    add component Privacy Policy

--- modules/privacy-policy/index.js
+++ modules/privacy-policy/index.js
@@ -1,43 +1,63 @@
-import { StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
-import { Text, View, ScrollView, TouchableOpacity, useWindowDimensions } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  useWindowDimensions
+} from "react-native";
 import { styles } from "./styles";
 import HTML from "react-native-render-html";
 import { unwrapResult } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { fetchPrivacy, slice } from "./store";
 
-const PrivacyPolicy = ({
-  navigation,
-  headingContainerStyle = {},
-  headingTextStyle = {},
-  contentContainerStyle = {}
-}) => {
+const PrivacyPolicy = ({ navigation, headingContainerStyle = {}, headingTextStyle = {}, contentContainerStyle = {} }) => {
   const contentWidth = useWindowDimensions().width;
-  const [htmlContent, setHtmlContent] = useState("<h3> Loading Privacy Policy... </h3>");
+  const [htmlContent, setHtmlContent] = useState(
+    "<h3> Loading Privacy Policy... </h3>"
+  );
   const dispatch = useDispatch();
+
   useEffect(async () => {
-    await dispatch(fetchPrivacy()).then(unwrapResult).then(res => {
-      setHtmlContent(res[0]?.body);
-    }).catch(err => {
-      console.log(err);
-      setHtmlContent("<h3> Privacy Policy could not be loaded at this time.</h3>");
-      alert("Privacy Policy could not be loaded at this time.");
-    });
+    await dispatch(fetchPrivacy())
+      .then(unwrapResult)
+      .then((res) => {
+        setHtmlContent(res[0]?.body);
+      })
+      .catch((err) => {
+        console.log(err);
+        setHtmlContent(
+          "<h3> Privacy Policy could not be loaded at this time.</h3>"
+        );
+        alert("Privacy Policy could not be loaded at this time.");
+      });
   });
-  return <View style={_styles.KKkVpioN}>
+
+  return (
+    <View
+      style={{
+        flex: 1
+      }}
+    >
       <View style={[styles.heading, headingContainerStyle]}>
-        <TouchableOpacity style={styles.touchableopacity} onPress={() => {
-        navigation.goBack();
-      }}></TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchableopacity}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        ></TouchableOpacity>
         <Text style={[styles.header, headingTextStyle]}>Privacy Policy</Text>
       </View>
       <ScrollView style={styles.privacyContainer}>
-        <HTML source={{
-        html: htmlContent
-      }} contentWidth={contentWidth} style={[styles.heading, contentContainerStyle]} />
+        <HTML
+          source={{ html: htmlContent }}
+          contentWidth={contentWidth}
+          style={[styles.heading, contentContainerStyle]}
+        />
       </ScrollView>
-    </View>;
+    </View>
+  );
 };
 
 export default {
@@ -45,9 +65,3 @@ export default {
   navigator: PrivacyPolicy,
   slice
 };
-
-const _styles = StyleSheet.create({
-  KKkVpioN: {
-    flex: 1
-  }
-});
