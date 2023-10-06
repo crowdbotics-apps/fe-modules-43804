1d9d178e13bc7886fdde99d988af2a551b3176a6
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-10-06 19:38:50 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "fe-modules-43804",
-  url: "https://fe-modules-43804.botics.co",
-  api: "https://fe-modules-43804.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "fe-modules-43804",
+  "url": "https://fe-modules-43804.botics.co",
+  "api": "https://fe-modules-43804.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
