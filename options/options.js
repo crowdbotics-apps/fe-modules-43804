dceece28a4d6383e5ee16f82350ffec08bee550b
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-10-09 16:05:54 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -5,7 +5,7 @@ export const globalOptions = {
   "screenOptions": {
     "headerShown": false
   },
-  "initialRoute": "welcome"
+  "initialRoute": "Sadf"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
